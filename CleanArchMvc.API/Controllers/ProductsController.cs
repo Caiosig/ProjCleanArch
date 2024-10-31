18fbf1bd24b7e80ee000a6341dfe218f37598062
Caiosig <caio_italy@hotmail.com>
2024-10-31 11:40:34 -0300

    Adicionando a API REstFul no projeto, criado os controllers necessário e feito todos os teste via swagger

---
+++ CleanArchMvc.API/Controllers/ProductsController.cs
@@ -0,0 +1,80 @@
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productsService;
+
+        public ProductsController(IProductService productsService)
+        {
+            _productsService = productsService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> Get()
+        {
+            var products = await _productsService.GetProduts();
+
+            if (products == null)
+                return NotFound("Products not found");
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}", Name = "GetProducts")]
+        public async Task<ActionResult<ProductDTO>> GetById(int? id)
+        {
+            var products = await _productsService.GetById(id);
+
+            if (products == null)
+                return NotFound("Products not found");
+
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] ProductDTO productDTO)
+        {
+            if (productDTO == null)
+                return BadRequest("Invald Data");
+
+            await _productsService.Add(productDTO);
+
+            return new CreatedAtRouteResult("GetProducts", new { id = productDTO.Id }, productDTO);
+        }
+
+
+        [HttpPut]
+        public async Task<ActionResult> Put(int id, [FromBody] ProductDTO productDTO)
+        {
+            if (id != productDTO.Id)
+                return BadRequest();
+
+            if (productDTO == null)
+                return BadRequest();
+
+            await _productsService.Update(productDTO);
+
+            return Ok(productDTO);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ProductDTO>> Delete(int id)
+        {
+            var products = await _productsService.GetById(id);
+
+            if (products == null)
+                return NotFound("Products not found");
+
+            await _productsService.Delete(id);
+
+            return Ok(products);
+        }
+    }
+}
---
+++ CleanArchMvc.Infra.IoC/DependencyInjectionAPI.cs
@@ -0,0 +1,53 @@
+﻿using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Application.Mappings;
+using CleanArchMvc.Application.Services;
+using CleanArchMvc.Domain.Account;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using CleanArchMvc.Infra.Data.Identity;
+using CleanArchMvc.Infra.Data.Repositories;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchMvc.Infra.IoC
+{
+    public static class DependencyInjectionAPI
+    {
+
+        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                                                        options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                                                        b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            //Registro das classes de serviços padrões
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+
+            //Registrando os serviços de Autenticação
+            services.AddScoped<IAuthenticate, AuthenticateService>();
+
+            //Registrando o automapper do DomainDTO
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            //Adicionado o services para a parte de autenticação via ferramenta identity
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            //Referenciando os Handlers da camada de aplicação para utilização do MediatR
+            var myHandlers = AppDomain.CurrentDomain.Load("CleanArchMvc.Application");
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(myHandlers);
+            });
+
+            return services;
+        }
+    }
+}
